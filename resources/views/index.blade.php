1fc78696aac96f7eaae898fcf3c41ef88de9fb01
RevanSP <revanspstudy28@gmail.com>
2025-01-21 13:34:12 +0700

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>LARAVEL VERCEL</title>
+
+</head>
+
+<body>
+    <h2>Response Headers :</h2>
+    <pre>
+        <?php
+        $headers = headers_list();
+        
+        foreach ($headers as $header) {
+            echo $header . '<br>';
+        }
+        ?>
+    </pre>
+
+    <p>
+        You must add the <code>APP_KEY</code> from your <code>.env</code> file into the <code>vercel.json</code>
+        configuration.
+        To do this, you need to set the <code>APP_KEY</code> in the <code>env</code> section of the
+        <code>vercel.json</code> file.
+        First, generate your <code>APP_KEY</code> using the command <code>php artisan key:generate</code> and then copy
+        the generated key into the <code>vercel.json</code> file like this:
+    </p>
+
+    <pre>
+    {
+        "version": 2,
+        "framework": null,
+        "functions": {
+            "api/index.php": { "runtime": "vercel-php@0.6.0" }
+        },
+        "routes": [
+            {
+                "src": "/(.*)",
+                "dest": "/api/index.php"
+            },
+            {
+                "src": "/(.*)",
+                "dest": "public/$1"
+            }
+        ],
+        "env": {
+            "APP_ENV": "production",
+            "APP_DEBUG": "true",
+            "APP_URL": "https://yourprojectdomain.com",
+            "APP_KEY": "",                                  // Add your APP_KEY here
+            "APP_CONFIG_CACHE": "/tmp/config.php",
+            "APP_EVENTS_CACHE": "/tmp/events.php",
+            "APP_PACKAGES_CACHE": "/tmp/packages.php",
+            "APP_ROUTES_CACHE": "/tmp/routes.php",
+            "APP_SERVICES_CACHE": "/tmp/services.php",
+            "VIEW_COMPILED_PATH": "/tmp",
+    
+            "CACHE_DRIVER": "array",
+            "LOG_CHANNEL": "stderr",
+            "SESSION_DRIVER": "cookie"
+        }
+    }
+    </pre>
+</body>
+
+</html>
